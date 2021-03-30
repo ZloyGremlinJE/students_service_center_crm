3d5b1eeb931135fc038f2f51e3f51fcedd7f4917
Semyon_Fomichyov <fomichyov.semyon@gmail.com>
2021-03-30 15:25:12 +0300

    Init.

---
+++ src/main/java/com/jm/students/ServiceCenterCrmApplication.java
@@ -0,0 +1,13 @@
+package com.jm.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCenterCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCenterCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/students/ServiceCenterCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCenterCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
