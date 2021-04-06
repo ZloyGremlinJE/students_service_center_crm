b94a69fe9394d2fefaed350f05c2ba696e56948b
ZloyGremlinJE <rellepro@gmail.com>
2021-04-06 16:46:29 +0300

    Formatted code ServiceCenterCrmApplication

--- src/main/java/com/jm/students/ServiceCenterCrmApplication.java
+++ src/main/java/com/jm/students/ServiceCenterCrmApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceCenterCrmApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ServiceCenterCrmApplication.class, args);
     }
 
