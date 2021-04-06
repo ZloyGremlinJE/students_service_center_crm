c6c46afc85e0ee246d394e61720fd6305087a1d3
ZloyGremlinJE <rellepro@gmail.com>
2021-04-06 16:44:20 +0300

    Refactor ServiceCenterCrmApplication

--- src/main/java/com/jm/students/ServiceCenterCrmApplication.java
+++ src/main/java/com/jm/students/ServiceCenterCrmApplication.java
@@ -1,18 +1,14 @@
 package com.jm.students;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class ServiceCenterCrmApplication {
-	@Autowired
-	JavaMailSender mailSender;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(ServiceCenterCrmApplication.class, args);
-	}
+        SpringApplication.run(ServiceCenterCrmApplication.class, args);
+    }
 
 }
