9d58556164c858bc09b36d77cf5dbb7cc2da88e5
Igor Belogolovsky <igor@belogolovsky.net>
2021-04-07 06:55:53 +0300

    Added Parsing JSON (unfinished)

--- src/main/java/com/jm/students/service/util/GeocodingService.java
+++ src/main/java/com/jm/students/service/util/GeocodingService.java
@@ -1,9 +1,12 @@
 package com.jm.students.service.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jm.students.model.Location;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public interface GeocodingService {
-    Location getLocation(String address);
+    Optional<Location> getLocation(String address) throws JsonProcessingException;
 }
--- src/main/java/com/jm/students/service/util/GeocodingServiceHereImpl.java
+++ src/main/java/com/jm/students/service/util/GeocodingServiceHereImpl.java
@@ -1,10 +1,45 @@
 package com.jm.students.service.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jm.students.model.Location;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-public class GeocodingServiceHereImpl implements GeocodingService{
+import java.util.Optional;
+
+public class GeocodingServiceHereImpl implements GeocodingService {
+
+    private String hereMapsApiKey = "vLzuZVgjeNRt1lGlVutRAQWCli2hPzcHenxs9gcxdtk";
+
+    private String hereMapsApiUrl = "https://geocode.search.hereapi.com/v1/geocode";
+
+    public void setHereMapsApiUrl(String hereMapsApiUrl) {
+        this.hereMapsApiUrl = hereMapsApiUrl;
+    }
+
+    public void setHereMapsApiKey(String hereMapsApiKey) {
+        this.hereMapsApiKey = hereMapsApiKey;
+    }
+
     @Override
-    public Location getLocation(String address) {
-        return null;
+    public Optional<Location> getLocation(String address) throws JsonProcessingException {
+        Optional<Location> result = Optional.empty();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = hereMapsApiUrl +
+                "?q=" +
+                address.replaceAll(" ", "+") +
+                "&apiKey=" +
+                hereMapsApiKey;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode root = objectMapper.readValue(response.getBody(), JsonNode.class);
+        }
+
+
+        return result;
     }
 }
