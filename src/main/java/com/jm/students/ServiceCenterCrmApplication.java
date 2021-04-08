0993a1933bf0937d67f397cd9bc182d5632daf3d
Denis Akulov <ads83@yandex.ru>
2021-04-08 19:46:58 +0300

    Commit 2

--- src/main/java/com/jm/students/ServiceCenterCrmApplication.java
+++ src/main/java/com/jm/students/ServiceCenterCrmApplication.java
@@ -1,12 +1,8 @@
 package com.jm.students;
 
-
-import com.jm.students.service.quartz.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ServiceCenterCrmApplication {
