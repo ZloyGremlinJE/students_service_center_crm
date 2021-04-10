07ac87050c9c601d07661737839f298ed89f40bf
Maksim Khaustov <haustovmaksim13@gmail.com>
2021-04-10 11:40:40 +0300

    add enum status

---
+++ src/main/java/com/jm/students/enums/StatusRequestType.java
@@ -0,0 +1,12 @@
+package com.jm.students.enums;
+
+/**
+ * Перечисление для состояний заявок в сервисном центре
+ */
+public enum StatusRequestType {
+    NEW,
+    CONFIRMED_BY_CLIENT,
+    IN_WORK,
+    COMPLETE,
+    CANCELED
+}
