86b9b8ac8a50ab4de16ce34f49f46e2457afde83
vladeg1977 <73729840+vladeg1977@users.noreply.github.com>
2021-04-17 18:07:32 +0700

    Добавил новый chatBotMethod в TelegramBot

--- src/main/java/com/jm/students/bot/TelegramBot.java
+++ src/main/java/com/jm/students/bot/TelegramBot.java
@@ -1,21 +1,36 @@
 package com.jm.students.bot;
 
+import com.jm.students.model.ServiceRequest;
+import com.jm.students.model.User;
+import com.jm.students.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
 
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+
+    private final UserService userService;
+
 
     @Value("${bot.name}")
     private String botUsername;
 
     @Value("${bot.token}")
     private String botToken;
+
+    @Autowired
+    public TelegramBot(UserService userService) {
+        this.userService = userService;
+    }
 
 
     @Override
@@ -29,20 +44,52 @@ public class TelegramBot extends TelegramLongPollingBot {
         return botToken;
     }
 
+
+
+
     @Override
     public void onUpdateReceived(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        String inputText = update.getMessage().getText();
+        Message message = update.getMessage();
 
-        if (inputText.startsWith("/start")) {
-            SendMessage message = new SendMessage();
-            message.setChatId(String.valueOf(chatId));
-            message.setText("Привет, это BestCRMBot!!!!!");
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/start":
+                    sendMessage(message, "Привет, это BestCRMBot!!!!!");
+                    break;
+                case "/hello":
+                    sendMessage(message, "Чем могу помочь?");
+                    break;
+                case "/service":
+                    sendMessageFromRequest(message);
+                    break;
             }
+        }
+    }
+
+
+    public void sendMessage(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessageFromRequest(Message message) {
+        SendMessage sendMessage = new SendMessage();
+        User userFromDb = userService.getUserByTelegramId(String.valueOf(message.getChatId()));
+        List<ServiceRequest> requestList = userFromDb.getRequestList();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(userFromDb.getTelegramChatId());
+        sendMessage.setText(String.valueOf(requestList));
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/jm/students/model/User.java
+++ src/main/java/com/jm/students/model/User.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -28,4 +30,28 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer", fetch = FetchType.EAGER)
+    List<ServiceRequest> requestList = new ArrayList<>();
+
+    public void addNewServiceRequest(ServiceRequest request) {
+        requestList.add(request);
+        request.setCustomer(this);
+    }
+
+    public void removeServiceRequest(ServiceRequest request) {
+        requestList.remove(request);
+        request.setCustomer(null);
+    }
+
+
+    private String telegramChatId;
+
+    @Override
+    public String toString() {
+        return "работник сервиса : " +
+                " имя : " + firstName + '\'' +
+                " фамилия : " + lastName + '\'' +
+                " должность : " + role;
+    }
 }
