fe898625bb25ce534dfc9c2539c238689bf742de
vladeg1977 <73729840+vladeg1977@users.noreply.github.com>
2021-04-19 22:51:55 +0700

    sendMessageRequest(User user, ServiceRequest request)

--- src/main/java/com/jm/students/bot/TelegramBot.java
+++ src/main/java/com/jm/students/bot/TelegramBot.java
@@ -10,8 +10,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -71,8 +75,10 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
         try {
+            setButtons(sendMessage);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
@@ -85,7 +91,36 @@ public class TelegramBot extends TelegramLongPollingBot {
         List<ServiceRequest> requestList = userFromDb.getRequestList();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(userFromDb.getTelegramChatId());
+        sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(String.valueOf(requestList));
+        try {
+            setButtons(sendMessage);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    public void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(keyboardMarkup);
+        keyboardMarkup.setSelective(true);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRow.add(new KeyboardButton("/start"));
+        keyboardRow.add(new KeyboardButton("/hello"));
+        keyboardRow.add(new KeyboardButton("/service"));
+
+        keyboardRowList.add(keyboardRow);
+        keyboardMarkup.setKeyboard(keyboardRowList);
+    }
+
+    public void sendMessageRequest(User user, ServiceRequest request) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(user.getTelegramChatId());
+        sendMessage.setText(request.toString());
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
